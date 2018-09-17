cac3bfbf94eba97a7293d2b22177b4cfd746342e
Ma7eer <maher.alkendi2@gmail.com>
2018-09-17 05:02:36 -0600

    now only works for youtube pages (temp solution)

--- popup.js
+++ popup.js
@@ -4,18 +4,22 @@ let videoId;
 const getVideoId = url => url.slice(url.indexOf(uniqueChar) + 2);
 
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-  videoId = getVideoId(tabs[0].url);
+  if (tabs[0].url.includes(uniqueChar)) {
+    videoId = getVideoId(tabs[0].url);
 
-  // http request
-  // inspect body of chrome extension to see the result in the console
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", `https://commentiment.herokuapp.com/sentiment/yt?id=${videoId}`, true);
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4) {
-      const response = JSON.parse(xhr.responseText);
-      // const response = JSON.stringify({ "data" : xhr.responseText });
-      console.log(response);
+    // http request
+    // inspect body of chrome extension to see the result in the console
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", `https://commentiment.herokuapp.com/sentiment/yt?id=${videoId}`, true);
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState == 4) {
+        const response = JSON.parse(xhr.responseText);
+        // const response = JSON.stringify({ "data" : xhr.responseText });
+        console.log(response);
+    }
   }
-}
-  xhr.send();
+    xhr.send();
+  } else {
+    console.log('wrong page');
+  }
 });
