cb67f1b6899db17457fb9e88c5cc01d760071939
Ma7eer <maher.alkendi2@gmail.com>
2018-09-17 04:49:43 -0600

    app can now make http requests to aommentiment-api

--- popup.js
+++ popup.js
@@ -5,5 +5,17 @@ const getVideoId = url => url.slice(url.indexOf(uniqueChar) + 2);
 
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
   videoId = getVideoId(tabs[0].url);
-  document.querySelector('h1').innerHTML = videoId;
+
+  // http request
+  // inspect body of chrome extension to see the result in the console
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", `https://commentiment.herokuapp.com/sentiment/yt?id=${videoId}`, true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+      const response = JSON.parse(xhr.responseText);
+      // const response = JSON.stringify({ "data" : xhr.responseText });
+      console.log(response);
+  }
+}
+  xhr.send();
 });
