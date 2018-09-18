0963e69ba40d1d489d970d1183ade33ad81b7e1a
Ma7eer <maher.alkendi2@gmail.com>
2018-09-18 04:10:06 -0600

    refactored popup.js

--- popup.js
+++ popup.js
@@ -1,56 +1,67 @@
 const uniqueChar = 'v=';
-let videoId;
-let resArray = [];
 
 const getVideoId = url => url.slice(url.indexOf(uniqueChar) + 2);
 
+const createDoughnutChart = (positive, negative) => {
+  var ctx = document.getElementById("myChart").getContext("2d");
+  var myDoughnutChart = new Chart(ctx,{
+    type: "doughnut",
+    data: {
+      labels: [ "Positive", "Negative"],
+      datasets: [{
+          label: "Youtube Comment Sentiment",
+          backgroundColor: ["green", "red"],
+          borderColor: "white",
+          data: [positive, negative],
+      }]
+  },
+    options: {}
+});
+}
+
+const commentsDataCalcs = (positiveScores, negativeScores) => {
+  let sumPos = positiveScores.reduce((previous, current) => current += previous);
+  let avgPos = (sumPos / positiveScores.length);
+  let sumNeg = negativeScores.reduce((previous, current) => current += previous);
+  let avgNeg = (sumNeg / negativeScores.length);
+  let maxVal = avgPos + (-avgNeg);
+  let positive = (avgPos*(100/maxVal)).toFixed(0);
+  let negative = (-avgNeg*(100/maxVal)).toFixed(0);
+  return [positive, negative];
+}
+
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+  let videoId;
 
   if (tabs[0].url.includes(uniqueChar)) {
     videoId = getVideoId(tabs[0].url);
 
-    // http request
-    // inspect body of chrome extension to see the result in the console
+    // xmlhttp request
     var xhr = new XMLHttpRequest();
     xhr.open("GET", `https://commentiment.herokuapp.com/sentiment/yt?id=${videoId}`, true);
     xhr.onreadystatechange = function() {
       if (xhr.readyState == 4) {
         const response = JSON.parse(xhr.responseText);
-        let positive = [];
-        let negative = [];
-        let token =[];
+        let resArray = [];
+        let positiveScores = [];
+        let negativeScores = [];
         resArray.push(response);
-        resArray[0].forEach((id) => {
-          // console.log(typeof(id.score));
-          if(id.score > 0) {
-            positive.push(id.comparative);
+        resArray[0].forEach(val => {
+          if(val.score > 0) {
+            positiveScores.push(val.score);
           } else {
-            negative.push(id.comparative);
+            negativeScores.push(val.score);
           }
         });
-        let sumPos = positive.reduce((previous, current) => current += previous);
-        let avgPos = (sumPos / positive.length);
-        let sumNeg = negative.reduce((previous, current) => current += previous);
-        let avgNeg = (sumNeg / negative.length);
+        let percentPositive = commentsDataCalcs(positiveScores, negativeScores)[0];
+        let percentNegative = commentsDataCalcs(positiveScores, negativeScores)[1];
 
-        var ctx = document.getElementById('myChart').getContext('2d');
-        var myDoughnutChart = new Chart(ctx,{
-          type: 'doughnut',
-          data: {
-            labels: [ "Positive", "Negative"],
-            datasets: [{
-                label: "Youtube Comment Sentiment",
-                backgroundColor: ['green', 'red'],
-                borderColor: 'white',
-                data: [(avgPos*(100/5)).toFixed(2)*10, (-avgNeg*(100/5)).toFixed(2)*10],
-            }]
-        },
-          options: {}
-      });
+        createDoughnutChart(percentPositive, percentNegative);
     }
   }
     xhr.send();
   } else {
-    console.log('wrong page');
+    document.querySelector("h1").innerHTML = "Go to YouTube.com to use this app!";
+    document.getElementById("myChart").style.display = "none";
   }
 });
