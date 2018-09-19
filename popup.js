78aae5c8777b18abb0173356c23ffa86be457c4f
Ma7eer <maher.alkendi2@gmail.com>
2018-09-19 02:39:14 -0600

    refactored the way the video id parameter is found

--- popup.js
+++ popup.js
@@ -1,7 +1,14 @@
-const uniqueChar = 'v=';
+const videoIdQueryParamId = 'v';
 
-const getVideoId = url => url.slice(url.indexOf(uniqueChar) + 2);
+/* returns youtube video id from youtube url */
+const getVideoId = url => {
+  let usp = new URL(url);
+  let params = new URLSearchParams(usp.search);
+  let id = params.get('v');
+  return params.get('v');
+};
 
+/* uses chartsjs to create a doughnut chart*/
 const createDoughnutChart = (positive, negative) => {
   var ctx = document.getElementById("myChart").getContext("2d");
   var myDoughnutChart = new Chart(ctx,{
@@ -19,7 +26,10 @@ const createDoughnutChart = (positive, negative) => {
 });
 }
 
+/* returns an array with two values: percent of positive comments and percent of
+negative ones */
 const commentsDataCalcs = (positiveScores, negativeScores) => {
+  if(positiveScores.length > 0 && negativeScores.length > 0) {
   let sumPos = positiveScores.reduce((previous, current) => current += previous);
   let avgPos = (sumPos / positiveScores.length);
   let sumNeg = negativeScores.reduce((previous, current) => current += previous);
@@ -28,12 +38,17 @@ const commentsDataCalcs = (positiveScores, negativeScores) => {
   let positive = (avgPos*(100/maxVal)).toFixed(0);
   let negative = (-avgNeg*(100/maxVal)).toFixed(0);
   return [positive, negative];
+  } else if (positiveScores.length === 0) {
+    return [0, 100];
+  } else {
+    return [100, 0];
+  }
 }
 
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
   let videoId;
 
-  if (tabs[0].url.includes(uniqueChar)) {
+  if (tabs[0].url.includes(videoIdQueryParamId + '=')) {
     videoId = getVideoId(tabs[0].url);
 
     // xmlhttp request
@@ -61,6 +76,7 @@ chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
   }
     xhr.send();
   } else {
+    /* if not on a YouTube page */
     document.querySelector("h1").innerHTML = "Go to YouTube.com to use this app!";
     document.getElementById("myChart").style.display = "none";
   }
