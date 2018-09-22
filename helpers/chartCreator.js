721be6f4ae6a8643455062a2bf5db8a836a63a1b
Ma7eer <maher.alkendi2@gmail.com>
2018-09-22 13:55:31 -0600

    added bootstrap + added carousal

--- helpers/chartCreator.js
+++ helpers/chartCreator.js
@@ -1,5 +1,5 @@
 export const createDoughnutChart = (positive, negative) => {
-  var ctx = document.getElementById("myChart").getContext("2d");
+  var ctx = document.getElementById("myChart1").getContext("2d");
   var myDoughnutChart = new Chart(ctx,{
     type: "doughnut",
     data: {
@@ -14,3 +14,33 @@ export const createDoughnutChart = (positive, negative) => {
     options: {}
 });
 }
+
+var ctx2 = document.getElementById('myChart2').getContext('2d');
+var ctx3 = document.getElementById('myChart3').getContext('2d');
+var data = {
+  datasets: [{
+      data: [10, 20, 30],
+      backgroundColor: ["green", "red", "blue"]
+  }],
+
+  // These labels appear in the legend and in the tooltips when hovering different arcs
+  labels: [
+      'Red',
+      'Yellow',
+      'Blue'
+  ]
+};
+
+// For a pie chart
+var myPieChart = new Chart(ctx2,{
+  type: 'pie',
+  data: data,
+  options: {}
+});
+
+// And for a doughnut chart
+var myDoughnutChart = new Chart(ctx3, {
+  type: 'doughnut',
+  data: data,
+  options: {}
+});
